25ab06e7685a324ac9c95c403f4eb8d48afb8eda
Jon Flores <jflore61@ucsc.edu>
2024-09-21 00:48:07 -0700

    custom h1-h5 added to light and dark mode in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,15 @@ import { ModeToggle } from "./components/mode-toggle";
 export default function Home() {
   return (
     <div>
-      <ModeToggle/>
+      <ModeToggle />
       <h1>Hello From the Home Page</h1>
       <h2>This is an H2</h2>
       <h3>This is an H3</h3>
       <h4>This is an H4</h4>
-      <Link href={"/"}>This is a Link component</Link>
+      <h5>This is an H5</h5>
+      <Link className="Link" href={"/"}>
+        This is a Link component
+      </Link>
     </div>
   );
 }
