83185d9c16717410aca0ccedee72c080ed260141
Jon Flores <jflore61@ucsc.edu>
2024-12-17 17:59:57 -0800

    id added to Language Selector component select tag

--- src/app/components/TopNavigation/LanguageSelector.tsx
+++ src/app/components/TopNavigation/LanguageSelector.tsx
@@ -6,18 +6,21 @@ import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 
 const LanguageSelector: FC<{ locale: string }> = ({ locale }) => {
+  const pathname = usePathname();
+  const router = useRouter();
 
-    const pathname = usePathname();
-    const router = useRouter();
-
-    const handleLanguageChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const newLocale = e.target.value as string;
-        const path = pathname.split("/").slice(2).join("/");
-        router.push(`/${newLocale}/${path}`);
-      };
+  const handleLanguageChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const newLocale = e.target.value as string;
+    const path = pathname.split("/").slice(2).join("/");
+    router.push(`/${newLocale}/${path}`);
+  };
   return (
     <Fragment>
-      <select value={locale} onChange={handleLanguageChange}>
+      <select
+        id={"languageSelector"}
+        value={locale}
+        onChange={handleLanguageChange}
+      >
         <option value={"en"}>En</option>
         <option value={"es"}>Es</option>
       </select>
